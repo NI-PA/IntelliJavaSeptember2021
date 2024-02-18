7aac948c1bcd4e541a6caa71812281bf881785c4
NI-PA <91149653+NI-PA@users.noreply.github.com>
2024-02-18 12:12:11 +0530

    Update susmita511_12_2_triangle.java

--- src/susmita511_12_2_triangle.java
+++ src/susmita511_12_2_triangle.java
@@ -15,7 +15,7 @@ public class susmita511_12_2_triangle extends susmita511_12_2 {
         System.out.println("---------Triangle Details---------");
         System.out.print("The length of the 3 sides are : " + sides[0] + " " + sides[1] + " " + sides[2]);
         clacArea();
-        System.out.print("\nThe area is : " + area);
+        System.out.print("\nThe area is : " + area );
         System.out.println();
     }
 
